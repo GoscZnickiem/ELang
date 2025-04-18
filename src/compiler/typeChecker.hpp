4ef2d256fc7398ba3a128cdd7563fa674cae713b
GoscZnickiem <maciek.6098@gmail.com>
2025-04-18 15:11:57 +0200

    nothing important

---
+++ src/compiler/typeChecker.hpp
@@ -0,0 +1,11 @@
+#ifndef _ELC_COMPILER_TYPECHECKER_
+#define _ELC_COMPILER_TYPECHECKER_
+
+class TypeChecker {
+public:
+	TypeChecker();
+
+
+};
+
+#endif // !_ELC_COMPILER_TYPECHECKER_
---
+++ src/compiler/types.hpp
@@ -0,0 +1,14 @@
+#ifndef _ELC_COMPILER_TYPES_
+#define _ELC_COMPILER_TYPES_
+
+class CompiledType {
+
+};
+
+class GenericIntegerType {
+
+};
+
+
+
+#endif // !_ELC_COMPILER_TYPES_
